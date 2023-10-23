2bd99e9a6d91da396efb9e041891e4058f7ed7b1
bzap <8540602+bzap@users.noreply.github.com>
2023-10-22 23:31:42 -0400

    Header bar updates (#2)
    * updated text styling and compacted header size
    
    * updated hero page to accomodate header bar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Container } from '@chakra-ui/react';
+import { Container, Flex } from '@chakra-ui/react';
 
 const Layout = ({ children }) => { 
     return (    
-        <Container  
-            maxW='100%' 
-            width='100%' 
-            margin='auto' 
-            minH='120vh' 
-            bg='#F2F2F2'
-            overflow={'hidden'}
-            userSelect={'none'}
-            left={0}
-            pl={0}
-            pr={0}> 
-                <main>{children}</main> 
-        </Container> 
+        <Flex
+            w={'100%'}
+            bg='#F2F2F2'>
+                <Container
+                    maxW='80em' 
+                    width='100%' 
+                    margin='auto' 
+                    minH='120vh' 
+                    overflow={'hidden'}
+                    userSelect={'none'}
+                    left={0}
+                    pl={0}
+                    pr={0}> 
+                        <main>{children}</main> 
+                </Container> 
+        </Flex>
     )   
 }
 
